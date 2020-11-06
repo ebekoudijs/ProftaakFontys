502616726920755772042c9d526da17ca75485fa
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2020-11-06 15:12:08 +0100

    Namen verbeterd

--- components/layout.js
+++ components/layout.js
@@ -9,7 +9,7 @@ import dynamic from 'next/dynamic'
 export const siteTitle = 'Insect Blend'
 export const filler = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-const DynamicComponentWithNoSSR = dynamic(() => import('../components/darkmode'), {
+const DarkMode = dynamic(() => import('../components/darkmode'), {
     ssr: false
 })
 
@@ -31,13 +31,12 @@ export default function Layout({ children, title, nofooter, noheader, nosidenav,
           <div className={styles.backToHome}>
             <Link href="/">
               <a>← Back to home</a>
-                            </Link>
-                            
+            </Link>             
           </div>)}
         </div>
       </div>
-            {!nofooter ? (<Footer />) : (<></>)}
-            <DynamicComponentWithNoSSR />
+      {!nofooter ? (<Footer />) : (<></>)}
+      <DarkMode />
     </div>
   )
 }
