50ccc1b2e3e7a7037ae56f45ff1d0058c8b095ba
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2020-11-09 15:10:14 +0100

    Philip img toegevoegd

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,8 @@
 import Head from 'next/head'
 import Layout, {filler} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-const pageTitle = "About"
+import Image from 'next/image'
+const pageTitle = "Over ons"
 
 
 export default function FirstPost() {
@@ -11,8 +12,13 @@ export default function FirstPost() {
       <Layout title={pageTitle}>
         <Head>
         </Head>
-        <section className={utilStyles.headingMd}>
-          <p>{filler}</p>
+              <section className={utilStyles.headingMd}>
+                  <Image
+                      src="/images/philip.png"
+                      alt="Picture of the author"
+                      width={500}
+                      height={500}
+                  />
         </section>
       </Layout> 
     </>
