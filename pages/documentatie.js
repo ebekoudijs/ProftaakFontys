9aa6c2bdbef716c32ec2c10d1965d7a5d3aaa341
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2020-11-09 13:28:06 +0100

    Documentatie pagina toegevoegd

---
+++ pages/documentatie.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout, {filler} from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+const pageTitle = "Documentatie"
+
+
+export default function FirstPost() {
+  return (
+    <>
+      <Layout title={pageTitle}>
+
+      </Layout> 
+    </>
+  )
+}
