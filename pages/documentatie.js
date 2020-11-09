00c9e3a2d58222f11ae04c0956b4a70cf7f08242
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2020-11-09 13:49:44 +0100

    Nutteloze imports verwijderd

--- pages/documentatie.js
+++ pages/documentatie.js
@@ -1,7 +1,4 @@
-import Link from 'next/link'
-import Head from 'next/head'
 import Layout, {filler} from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
 const pageTitle = "Documentatie"
 
 
