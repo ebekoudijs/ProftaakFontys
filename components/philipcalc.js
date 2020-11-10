5a2e9a6f2129f7de7583d047594464ac1464f746
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2020-11-10 10:41:22 +0100

    Result formatting

--- components/philipcalc.js
+++ components/philipcalc.js
@@ -2,43 +2,31 @@
 export default function philipcalc() {
 
     function bereken(radioWaarde, hoeveelheid, vlees, vleestype) {
-        var textboxOutput = document.getElementById("uitkomst");
-        var textboxBesparing = document.getElementById("besparing");
-        var proteine = document.getElementById("proteine");
-
         var proteValue = 0.0186 * hoeveelheid - vlees * hoeveelheid;
         var water = radioWaarde * hoeveelheid;
         var besparingValue = water - hoeveelheid * 4.341;
+        document.getElementById("result").innerHTML = "Voor de productie van " + "<b>" + hoeveelheid + " gram " + vleestype + "</b>" + " is " + "<b>" + water.toFixed(2) + " liter water " + "</b>" + "nodig."
+            + " Dat is " + "<b>" + besparingValue.toFixed(2) + " liter " + "</b>" +"meer/minder dan bij meelwormen nodig is."
 
-        document.getElementById("result").innerHTML = "Voor de productie van " + "<b>" + hoeveelheid + " gram " + vleestype + "</b>" + " is " + "<b>"+ water.toFixed(2) + " liter water " + "</b>" + "meer nodig vergeleken met dezelde massa meelwormen."
-            + " Ook zit er " + "<b>" + proteValue.toFixed(2) + " gram proteïne " + "</b>" + "meer in meelwormen dan in " + vleestype;
-
+            + " Ook zit er " + "<b>" + proteValue.toFixed(2) + " gram proteïne " + "</b>" + "meer in meelwormen dan in " + vleestype + " bij deze hoeveelheid.";
     }
     function vleesSelect() {
         var radio1 = document.getElementById("radio1").value;
         var radio2 = document.getElementById("radio2").value;
         var radio3 = document.getElementById("radio3").value;
         var textboxInput = document.getElementById("textHoeveelheid");
-        var textboxOutput = document.getElementById("uitkomst");
-        var textboxBesparing = document.getElementById("besparing");
-        var proteine = document.getElementById("proteine");
         var hoeveelheid = textboxInput.value;
         var proteHoeveelheid = [0.0138, 0.0105, 0.0127];
 
-
         if (document.getElementById("radio1").checked == true) {
             bereken(radio1, hoeveelheid, proteHoeveelheid[0], "rundvlees");
-            
         }
-
         else if (document.getElementById("radio2").checked == true) {
             bereken(radio2, hoeveelheid, proteHoeveelheid[1], "varkensvlees");
         }
-
         else if (document.getElementById("radio3").checked == true) {
             bereken(radio3, hoeveelheid, proteHoeveelheid[2], "kip");
         }
-
         else {
             document.getElementById("result").innerHTML = "Voer geldige gegevens in";
         }
@@ -47,7 +35,8 @@ export default function philipcalc() {
 
     return (
         <>
-            <label htmlFor="textHoeveelheid">hoeveelheid vlees in gram: </label>
+            <h1>hoeveelheid vlees in gram: </h1>
+            
             <input type="text" id="textHoeveelheid" name="textHoeveelheid"></input>
             <br />
             <input type="radio" id="radio1" name="group1" value="15.415"></input>
@@ -61,8 +50,7 @@ export default function philipcalc() {
                 <p> Bereken </p>
             </button>
             <p id="result"></p>
-
-
+            <p id ="test" />
         </>
     );
 }
