7fb8eb984e64e826f9cb52c9336d1d37f3da5baf
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2020-11-10 10:42:25 +0100

    Cookies toegevoegd
    Algemene functie voor setten, lezen en checken of cookie leeg is. Dark mode wordt nog niet automatisch geset op basis van cookies

--- components/darkmode.js
+++ components/darkmode.js
@@ -2,17 +2,66 @@
 
 
 export default function darkmode() {
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+    function checkCookie() {
+        var darkmode = getCookie("darkmode");
+        if (darkmode != "") {
+        }
+        else { setCookie("darkmode", false , 365); }
+    }
+
     return (
         <>
-                <button className={utilStyles.button} onClick={function () {
+            {checkCookie()}
+            <button className={utilStyles.button} onClick={function () {
+                var cookie = getCookie("darkmode");
+                if (cookie == 'false') { setCookie("darkmode", true, 365); }
+                else if (cookie == 'true') { setCookie("darkmode", false, 365); }
+
+            }}>
+                <h3>Dark mode cookie</h3>
+            </button>
+
+            
+
+
+
+
+
+
+            <button className={utilStyles.button} onClick={function () {
+                var cookie = document.cookie;
+
+
                 var element = document.getElementById("container");
                 var navbar = document.getElementById("navbarcontainer");
                 element.classList.toggle("dark-mode");
                 navbar.classList.toggle("dark-mode");
-                }} >
-            <h3>Dark mode</h3>
-            
-                </button>
+                //document.cookie = "darkMode=true; expires=Thu, 18 Dec 2025 12:00:00 UTC; path=/"; 
+
+            }} >
+                <h3>Dark mode</h3>
+            </button>
                
         </>
     );
