dad0894fadddbc8bdc793d88da306201c5dd3ca9
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2020-11-13 13:34:40 +0100

    p vervangen met span, input check

--- components/philipcalc.js
+++ components/philipcalc.js
@@ -1,4 +1,5 @@
 ﻿import utilStyles from '../styles/utils.module.css'
+import styles from '../styles/calc.module.css'
 export default function philipcalc() {
 
     function bereken(radioWaarde, hoeveelheid, vlees, vleestype) {
@@ -16,39 +17,47 @@ export default function philipcalc() {
         var textboxInput = document.getElementById("textHoeveelheid");
         var hoeveelheid = textboxInput.value;
         var proteHoeveelheid = [0.0138, 0.0105, 0.0127];
-
-        if (document.getElementById("radio1").checked == true) {
-            bereken(radio1, hoeveelheid, proteHoeveelheid[0], "rundvlees");
-        }
-        else if (document.getElementById("radio2").checked == true) {
-            bereken(radio2, hoeveelheid, proteHoeveelheid[1], "varkensvlees");
-        }
-        else if (document.getElementById("radio3").checked == true) {
-            bereken(radio3, hoeveelheid, proteHoeveelheid[2], "kip");
+        if (hoeveelheid == 0) {
+            document.getElementById("result").innerHTML = "Vul een geldige hoeveelheid in"
         }
         else {
-            document.getElementById("result").innerHTML = "Voer geldige gegevens in";
+            if (document.getElementById("radio1").checked == true) {
+                bereken(radio1, hoeveelheid, proteHoeveelheid[0], "rundvlees");
+            }
+            else if (document.getElementById("radio2").checked == true) {
+                bereken(radio2, hoeveelheid, proteHoeveelheid[1], "varkensvlees");
+            }
+            else if (document.getElementById("radio3").checked == true) {
+                bereken(radio3, hoeveelheid, proteHoeveelheid[2], "kip");
+            }
+            else {
+                document.getElementById("result").innerHTML = "Selecteer een van de drie opties";
+            }
         }
-
     }
 
     return (
         <>
+            <div className={styles.calcwrapper}>
             <h1>hoeveelheid vlees in gram: </h1>
             
-            <input type="text" id="textHoeveelheid" name="textHoeveelheid"></input>
+                <input type="number" id="textHoeveelheid" name="textHoeveelheid" className={styles.amountinput}></input>
             <br />
             <input type="radio" id="radio1" name="group1" value="15.415"></input>
-            <p>Rundvlees</p>
-            <input id="radio2" type="radio" name="group1" value="5.988"></input>
-            <p>Varkensvlees</p>
-            <input id="radio3" type="radio" name="group1" value="4.325"></input>
-            <p>Kip</p>
+                <span className={styles.radiospan}>Rundvlees</span>
                 <br />
+            <input id="radio2" type="radio" name="group1" value="5.988"></input>
+                <span className={styles.radiospan}>Varkensvlees</span>
+                <br />
+            <input id="radio3" type="radio" name="group1" value="4.325"></input>
+                <span className={styles.radiospan}>Kip</span>
+                <br />
+            <br />
             <button className={utilStyles.button} onClick={vleesSelect}>
                 <p> Bereken </p>
             </button>
-            <p id="result"></p>
+                <p id="result"></p>
+            </div>
         </>
     );
 }
