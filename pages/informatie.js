ab8faf7489f15b57e6692ec0b6ac6b2756a6902c
ebekoudijs <71434309+ebekoudijs@users.noreply.github.com>
2020-11-13 09:13:16 +0100

    Verhoudingen van de afbeeldingen goed gemaakt, quality op 100 gezet

--- pages/informatie.js
+++ pages/informatie.js
@@ -13,58 +13,67 @@ export default function FirstPost() {
           <Layout title={pageTitle}>
               <div className={styles.row}>
                   <div className={styles.column }>
-                    <Image
+                      <Image
+                          quality={100}
                      className={styles.grafiek}
                      src="/images/R-1.png"
                      alt="Picture of the author"
-                     width={400}
-                     height={400}
+                     width={420}
+                     height={267}
                     />
                   </div>
                   <div className={styles.column}>
                       <Image
+                          quality={100}
                           className={styles.grafiek}
                           src="/images/R-2.png"
                           alt="Picture of the author"
-                          width={400}
-                          height={400}
-                          quality="100"
+                          width={420}
+                          height={266}
                       />
                   </div>
+              </div>
+              <div className={styles.row}>
                   <div className={styles.column}>
                       <Image
-                          className={styles.grafiek}
+                          quality={100}
+                          layout="fill"
                           src="/images/R-3.png"
                           alt="Picture of the author"
-                          width={400}
-                          height={400}
+                          width={420}
+                          height={267}
                       />
                   </div>
                   <div className={styles.column}>
                       <Image
+                          quality={100}
                           className={styles.grafiek}
                           src="/images/R-4.png"
                           alt="Picture of the author"
-                          width={400}
-                          height={400}
+                          width={420}
+                          height={264}
                       />
                   </div>
+              </div>
+              <div className={styles.row}>
                   <div className={styles.column}>
                       <Image
+                          quality={100}
                           className={styles.grafiek}
                           src="/images/R-5.png"
                           alt="Picture of the author"
-                          width={400}
-                          height={400}
+                          width={420}
+                          height={263}
                       />
                   </div>
                   <div className={styles.column}>
                       <Image
+                          quality={100}
                           className={styles.grafiek}
                           src="/images/R-6.png"
                           alt="Picture of the author"
-                          width={400}
-                          height={400}
+                          width={420}
+                          height={264}
                       />
                   </div>
 
